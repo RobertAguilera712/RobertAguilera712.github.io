046d4dab01d68697a612e1f210649dad09328167
Roberto Aguilera <rc7.aguilera@gmail.com>
2021-12-28 11:11:51 -0600

    animations improved

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-const cards = document.querySelectorAll(".card")
+const elements = document.querySelectorAll(".fade")
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        entry.target.classList.toggle("show", entry.isIntersecting);
+        entry.target.classList.toggle("normal", entry.isIntersecting);
     });
+}, {
+    threshold: 0.2
 }
 );
 
-cards.forEach(card => {
-    observer.observe(card)
+elements.forEach(element => {
+    observer.observe(element)
 })
